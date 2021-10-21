20d59c8f5ae886ff51a7e86db47f5a7b830f4139
Lovepreet <jsjosan3@gmail.com>
2021-10-21 23:57:37 +0530

    adding files

---
+++ PredictFromModel.py
@@ -0,0 +1,34 @@
+from application_logging.logger import App_Logger
+from ModelFinder import finder
+import pandas as pd
+class prediction:
+
+    def __init__(self):
+        self.file_read = open("ModelLogs/Model_Predict.txt", 'a+')
+        self.log_writer = App_Logger()
+
+    def predict_results(self,data):
+        try:
+            self.log_writer.log(self.file_read, 'Start of Prediction')
+            predictions = []
+            model_load=finder.ModelFinder()
+            model = model_load.load_model()
+
+            result=(model.predict(data))
+
+            self.log_writer.log(self.file_read, 'Model loaded successfully')
+            for res in result:
+                if res == 0:
+                    predictions.append('<=50K')
+                else:
+                    predictions.append('>50K')
+
+            final = pd.DataFrame(list(zip(predictions)), columns=['Predictions'])
+            #path = "Prediction_Output_File/Predictions.csv"
+            #final.to_csv(path, header=True,mode='a+')  # appends result to prediction file
+            #self.log_writer.log(self.file_read, 'End of Prediction')
+            return final
+        except Exception as ex:
+            self.log_writer.log(self.file_read, 'Error occurred while running the prediction!! Error:: %s' % ex)
+
+
---
+++ main.py
@@ -0,0 +1,104 @@
+import os
+import pandas as pd
+from flask import Flask, request, render_template,Response
+from flask_cors import cross_origin,CORS
+import ReadData
+from DataSplitting import DataSplitting
+from Training_Data_Traonsfrmation import Preprocessing
+import numpy as np
+from ModelFinder.finder import ModelFinder
+from predictionGetData import Data_Getter_Pred
+from PredictFromModel import prediction
+from wsgiref import simple_server
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+@cross_origin()
+def predict():
+    try:
+        if request.form is not None:
+            path=request.form['filepath']
+            data_obj=Data_Getter_Pred(path)
+            data=data_obj.get_data()
+            preprocess = Preprocessing.Preprocessor()
+            X = preprocess.removeExtraSpace(data)
+            columns_with_null_values, is_null_present = preprocess.columnsWithMissingVlaue(X)
+            if (is_null_present):
+                X = preprocess.imputeMissingValue(columns_with_null_values,X)
+            preprocess.removeUnwantedFeatures(X, ['education'])
+            X = preprocess.computeOutliars(X, 'fnlwgt')
+            X = preprocess.outliarsCompute(X, 'hours-per-week')
+            df_numeric = preprocess.scaleDownNumericFeatures(X)
+            df_category = preprocess.encodeCategoryFeatures(X)
+            df = pd.concat([df_numeric, df_category], axis=1)
+            predict=prediction()
+            path=predict.predict_results(df)
+            #path=predict.predict_results(df)
+            path=pd.concat([data, path], axis=1)
+            path.to_html("Table.htm")
+            html_file =path.to_html()
+            return html_file
+
+    except ValueError:
+        return Response("Error Occurred! %s" %ValueError)
+        #print("Error Occurred! %s" %ValueError)
+    except KeyError:
+        return Response("Error Occurred! %s" %KeyError)
+        #print(("Error Occurred! %s" %KeyError))
+    except Exception as e:
+        return Response("Error Occurred! %s" %e)
+        #print(("Error Occurred! %s" %e))
+
+
+
+
+@app.route('/train',methods=['POST'])
+@cross_origin()
+def train_predict():
+    try:
+        readdataobj = ReadData.ReadData()
+        data=readdataobj.readData()
+        '''datasplitobj=DataSplitting(data)
+        X_train, X_test, y_train, y_test = datasplitobj.split_data()'''
+
+        preprocess=Preprocessing.Preprocessor()
+        data=preprocess.removeExtraSpace(data)
+        X, y = preprocess.seperateDependentIndependentColumns(data)
+        y=y.map({'<=50K':0,'>50K':1})
+        columns_with_null_values , is_null_present=preprocess.columnsWithMissingVlaue(X)
+        if(is_null_present):
+            X=preprocess.imputeMissingValue(columns_with_null_values,X)
+        preprocess.removeUnwantedFeatures(X , ['education'])
+        X = preprocess.computeOutliars(X, 'fnlwgt')
+        X = preprocess.outliarsCompute(X, 'hours-per-week')
+        df_numeric=preprocess.scaleDownNumericFeatures(X)
+        df_category=preprocess.encodeCategoryFeatures(X)
+        
+        df=pd.concat([df_numeric,df_category], axis=1)
+        X,y = preprocess.handleImbalancedDataSet(df,y)
+        split_data = DataSplitting()
+        X_train, X_test, y_train, y_test = split_data.split_data(X,y)
+        
+        modelfinder=ModelFinder()
+        modelfinder.get_best_model(X_train, X_test, y_train, y_test)
+
+
+    except ValueError:
+        print("Value error occurred %s" %ValueError)
+
+    except KeyError:
+        print("Key error occurred %s" %KeyError)
+
+    except Exception as e:
+        print("Error Occurred while training %s" %e)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True,threaded=True)
